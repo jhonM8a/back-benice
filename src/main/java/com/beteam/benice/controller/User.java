9d99dccd76b3e5eeaa8b3c5addc7ea6043613e54
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-05-22 21:12:36 -0500

    init commit

---
+++ src/main/java/com/beteam/benice/controller/User.java
@@ -0,0 +1,22 @@
+package com.beteam.benice.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beteam.benice.model.UserAuthRequest;
+import com.beteam.benice.model.UserResponse;
+
+@RestController
+@RequestMapping("/benice/v1")
+public class User {
+
+	@PostMapping("/user/auth")
+	public UserResponse authUser(@RequestBody UserAuthRequest user) {
+		UserResponse response = new UserResponse();
+		response.setName("Jhon ochoa");
+		response.setLocation(user.getPassword()+user.getUser());
+		return response;
+	}
+}
